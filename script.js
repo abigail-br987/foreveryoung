22fab6beb686e93e6bbf3f259bc15eda2825f542
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-06-29 05:57:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,156 @@
+const animation = [ // para que aplauda
+    "personita1.png",
+    "personita2.png",
+    "personita3.png",
+    "personita4.png",
+    "personita5.png",
+    "personita7.png"
+];
+
+const wordText = document.querySelector(".word"),
+    pistaText = document.querySelector(".pista span"),
+    timeText = document.querySelector(".time b"),
+    inputField = document.querySelector("input"),
+    container2 = document.querySelector(".container2"),
+    refreshBtn = document.querySelector(".refresh-word"),
+    checkBtn = document.querySelector(".check-word"),
+    contentBox = document.querySelector(".container .content"),
+    startArea = document.querySelector(".startBtn"),
+    scoreArea = document.querySelector(".score"),
+    thumbnail = document.querySelector("#word_scramble_game #word_scramble_thumbnail"),
+    box_content = document.querySelector(".box-content");
+
+var cajita = document.getElementById("cajita");
+var character = document.getElementById("littlecharacter");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var box_text = document.getElementById("box-text");
+
+let correctWord, timer;
+let score = 0; 
+
+contentBox.style.display = "none";
+container2.style.display = "none";
+box_text.innerHTML = `¡Con 10 aciertos ganas! ¡Tú puedes!`;
+
+const initTimer = (maxTime) => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        cajita.style.display = "block";
+        box_content.classList.add("cajita-wrong");
+        box_text.innerHTML = `Tiempo agotado! <b>${correctWord.toUpperCase()}<b> era la palabra`;
+        endGame();
+    }, 1000);
+};
+
+const start = () => {
+    contentBox.style.display = "block";
+    startArea.style.display = "none";
+    thumbnail.style.display = "none";
+    container2.style.display = "block";
+    initGame(); 
+};
+
+const endGame = () => {
+    clearInterval(timer);
+    contentBox.style.display = "none";
+    box_content.classList.add("cajita-correct");
+    thumbnail.src = "paralaproxima-03.png"
+    thumbnail.style.display = "block";
+};
+
+const winGame = () => {
+    clearInterval(timer);
+    contentBox.style.display = "none";
+    box_content.classList.add("cajita-correct");
+    thumbnail.src = "felicidades-03.png"
+    thumbnail.style.display = "block";
+};
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    
+    wordText.innerText = wordArray.join("");
+    pistaText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+    scoreArea.innerHTML = score;
+
+    if (score > 9) {
+        winGame();
+    }
+};
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+
+    if (!userWord) { 
+        cajita.style.display = "block";
+        box_content.classList.remove("cajita-wrong");
+        box_content.classList.remove("cajita-correct");
+        return box_text.innerHTML = `Escribe una palabra para evaluar`;
+    }
+
+    if (userWord !== correctWord) { 
+        if (score >= 1) {
+            score = score - 1; 
+            scoreArea.innerHTML = score;
+        }
+        cajita.style.display = "block";
+        box_content.classList.add("cajita-wrong");
+        playAnimationsad();
+        initGame();
+        return box_text.innerHTML = `Oops! <b>${userWord}<b> no es la palabra`;
+        
+    } else {
+        cajita.style.display = "block";
+        box_content.classList.remove("cajita-wrong");
+        box_content.classList.add("cajita-correct");
+        box_text.innerHTML = `Si! <b>${correctWord.toUpperCase()}<b> es la palabra`;
+        score++;
+        playAnimation();
+        initGame();
+    }
+};
+
+
+const playAnimationsad = () => {
+    character.src = animation[5];
+    setTimeout(() => {
+        character.src = animation[0]; 
+    }, 1000); 
+};
+
+
+const playAnimation = () => {
+    let index = 0;
+    let loops = 0;
+    const maxLoops = 3;
+
+    const interval = setInterval(() => {
+        character.src = animation[index];
+        index = (index + 1) % animation.length;
+        if (index === 0) {
+            loops++;
+            if (loops === maxLoops) {
+                clearInterval(interval);
+                character.src = animation[0];
+            }
+        }
+    }, 50);
+    
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
---
+++ words.js
@@ -0,0 +1,158 @@
+let words = [
+    {
+        word: "adolescencia",
+        hint: "Etapa de transición entre la niñez y la edad adulta"
+    },
+    {
+        word: "autoestima",
+        hint: "Percepción que una persona tiene de su propio valor"
+    },
+    {
+        word: "consentimiento",
+        hint: "Aprobación libremente otorgada para realizar una actividad"
+    },
+    {
+        word: "sexualidad",
+        hint: "Aspecto de la vida relacionado con el sexo y las relaciones sexuales"
+    },
+    {
+        word: "tolerancia",
+        hint: "Respeto y aceptación de las diferencias entre personas"
+    },
+    {
+        word: "ansiedad",
+        hint: "Sentimiento de miedo o preocupación intensa"
+    },
+    {
+        word: "autocuidado",
+        hint: "Práctica de cuidar de uno mismo para mantener la salud"
+    },
+    {
+        word: "derechos",
+        hint: "Privilegios legales que todas las personas tienen"
+    },
+    {
+        word: "higiene",
+        hint: "Prácticas para mantener la salud y prevenir enfermedades"
+    },
+    {
+        word: "resiliencia",
+        hint: "Capacidad de recuperarse frente a la adversidad"
+    },
+    {
+        word: "violencia",
+        hint: "Uso de la fuerza para dañar a alguien o algo"
+    },
+    {
+        word: "amistad",
+        hint: "Relación de afecto entre personas"
+    },
+    {
+        word: "bienestar",
+        hint: "Estado de satisfacción y comodidad"
+    },
+    {
+        word: "convivencia",
+        hint: "Vida en común con otras personas"
+    },
+    {
+        word: "crecimiento",
+        hint: "Desarrollo físico y mental a lo largo del tiempo"
+    },
+    {
+        word: "dignidad",
+        hint: "Calidad de ser digno y merecedor de respeto"
+    },
+    {
+        word: "diversidad",
+        hint: "Variedad de características humanas en una sociedad"
+    },
+    {
+        word: "familia",
+        hint: "Grupo de personas relacionadas por parentesco"
+    },
+    {
+        word: "igualdad",
+        hint: "Estado de ser igual en derechos y oportunidades"
+    },
+    {
+        word: "infancia",
+        hint: "Período de la vida que abarca desde el nacimiento hasta la adolescencia"
+    },
+    {
+        word: "integridad",
+        hint: "Cualidad de una persona recta y honesta"
+    },
+    {
+        word: "madurez",
+        hint: "Etapa de desarrollo en la que una persona alcanza su pleno potencial"
+    },
+    {
+        word: "optimismo",
+        hint: "Actitud de ver y juzgar las cosas en su aspecto más positivo"
+    },
+    {
+        word: "paciencia",
+        hint: "Capacidad de soportar contratiempos con serenidad"
+    },
+    {
+        word: "respeto",
+        hint: "Consideración y valoración especial hacia alguien o algo"
+    },
+    {
+        word: "solidaridad",
+        hint: "Apoyo incondicional a causas o intereses ajenos"
+    },
+    {
+        word: "valores",
+        hint: "Principios que guían el comportamiento humano"
+    },
+    {
+        word: "voluntariado",
+        hint: "Trabajo de ayuda desinteresada realizado por una persona"
+    },
+    {
+        word: "adversidad",
+        hint: "Situación desfavorable o difícil"
+    },
+    {
+        word: "equilibrio",
+        hint: "Estado de armonía y estabilidad"
+    },
+    {
+        word: "gratitud",
+        hint: "Sentimiento de aprecio por algo recibido"
+    },
+    {
+        word: "honestidad",
+        hint: "Cualidad de ser sincero y justo"
+    },
+    {
+        word: "independencia",
+        hint: "Condición de no depender de otros"
+    },
+    {
+        word: "perseverancia",
+        hint: "Firmeza y constancia en la realización de algo"
+    },
+    {
+        word: "prueba",
+        hint: "Examen o evaluación para demostrar conocimientos o habilidades"
+    },
+    {
+        word: "responsabilidad",
+        hint: "Capacidad de responder por los propios actos"
+    },
+    {
+        word: "tenacidad",
+        hint: "Firmeza y perseverancia en los propósitos"
+    },
+    {
+        word: "tranquilidad",
+        hint: "Estado de calma y sosiego"
+    },
+    {
+        word: "voluntad",
+        hint: "Capacidad humana para decidir y actuar según el propio criterio"
+    }
+];
